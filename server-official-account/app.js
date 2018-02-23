9a21a932e70c10fd6c5321a003d4f2fd61703ed6
lijiantao <825067852@qq.com>
2018-02-23 16:03:54 +0800

    edit

--- server-official-account/app.js
+++ server-official-account/app.js
@@ -1,8 +1,19 @@
 /**
  * Created by web-01 on 2018/2/23.
  */
-const EXPRESS=require('express');
+const EXPRESS = require('express');
+const WECHAT =require('wechat');
 
+let config={
+  token:'weixin',
+    appid:'wx519e354b8c69057d',
+    encodingAESKey:'dxcfZDO9qhWqUSi3B6MwDuHNzM6RTYaXtChkHBQwFCW'
+};
 let app=new EXPRESS();
 
+app.use(EXPRESS.query());
+app.get('/',WECHAT(config,(req,res,next)=>{
+    let message = req.weixin;
+    console.log(message);
+}));
 app.listen(3000);
